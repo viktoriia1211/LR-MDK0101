dcd6239b98dd1defac02a2489a9d1fe3c0876a75
viktoriia <vikaap21@yandex.ru>
2026-09-20 17:46:53 +0300

    Добавлена функция GetHeight для ввода роста

---
+++ ibm/ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMI
+{
+    public static class Class1
+    {
+        public static double GetHeight()
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите рост (см):");
+                if (double.TryParse(Console.ReadLine(), out double height) && height > 0)
+                    return height;
+                Console.WriteLine("Ошибка! Введите положительное число");
+            }
+        }
+    }
+}
